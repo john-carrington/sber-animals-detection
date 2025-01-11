ddb09527899e7d718916ee74aa1a3db9823a42cd
colrosezx <kirillkovalev-2005@mail.ru>
2025-01-12 03:54:22 +0500

    add a function of carousel back+front

--- src/frontend/sber_project/src/imgs.js
+++ src/frontend/sber_project/src/imgs.js
@@ -1,5 +1,4 @@
-const carousel = document.querySelector(".carousel");
-const backgroundImage = document.querySelector(".bg-image");
+const carousel = document.getElementById('carousel');
 const leftArrow = document.querySelector(".left-arrow");
 const rightArrow = document.querySelector(".right-arrow");
 const modal = document.getElementById('modal');
@@ -7,49 +6,23 @@ const modalImg = document.getElementById('modal-img');
 const closeBtn = document.querySelector('.close');
 
 let currentIndex = 0;
-let prevIndex;
-const images = document.querySelectorAll(".carousel-image");
-const totalImages = images.length;
+let images;
+let totalImages;
 const imageWidth = 300;
+const carouselWidth = 900; // Фиксированная ширина контейнера карусели
 
 leftArrow.addEventListener("click", () => {
-    prevIndex = currentIndex;
-    currentIndex = (currentIndex - 1 + totalImages) % totalImages;
-    carousel.style.transform = `translateX(-${imageWidth}px)`;
-    carousel.insertBefore(images[currentIndex], carousel.firstChild);
-
-    setTimeout(() => {
-        carousel.style.transform = "";
-        carousel.classList.add("sliding-transition");
-        backgroundImage.src = images[currentIndex].src.slice(0, -3) + "1000";
-    }, 10);
-
-    setTimeout(() => {
-        carousel.classList.remove("sliding-transition");
-    }, 490);
+    if (totalImages > 5) {
+        currentIndex = (currentIndex - 1 + totalImages) % totalImages;
+        updateCarousel();
+    }
 });
 
 rightArrow.addEventListener("click", () => {
-    carousel.classList.add("sliding-transition");
-
-    prevIndex = currentIndex;
-    currentIndex = (currentIndex + 1) % totalImages;
-
-    carousel.style.transform = `translateX(-${imageWidth}px)`;
-    backgroundImage.src = images[currentIndex].src.slice(0, -3) + "1000";
-
-    setTimeout(() => {
-        carousel.appendChild(images[prevIndex]);
-        carousel.classList.remove("sliding-transition");
-        carousel.style.transform = "";
-    }, 500);
-});
-
-images.forEach(image => {
-    image.addEventListener('click', function() {
-        modal.style.display = 'block';
-        modalImg.src = this.src;
-    });
+    if (totalImages > 5) {
+        currentIndex = (currentIndex + 1) % totalImages;
+        updateCarousel();
+    }
 });
 
 closeBtn.addEventListener('click', function() {
@@ -64,17 +37,17 @@ window.addEventListener('click', function(event) {
 
 document.getElementById('uploadForm').addEventListener('submit', async function(event) {
     event.preventDefault(); // Предотвращаем стандартное поведение формы
-  
+
     const fileInput = document.getElementById('fileInput');
     const preview = document.getElementById('preview');
     const files = fileInput.files;
     const formData = new FormData();
-  
+
     // Добавляем файлы в FormData
     for (let i = 0; i < files.length; i++) {
         const file = files[i];
         formData.append('files', file);
-  
+
         // Отображаем предварительный просмотр изображений
         const reader = new FileReader();
         reader.onload = function(e) {
@@ -84,47 +57,75 @@ document.getElementById('uploadForm').addEventListener('submit', async function(
         };
         reader.readAsDataURL(file);
     }
-  
+
     // Отправляем файлы на сервер
     try {
+        alert('Соединение устанавливается, пожалуйста подождите ответа');
         const response = await fetch('http://127.0.0.1:8000/api/predict/', {
             method: 'POST',
             body: formData
         });
-  
+
         if (!response.ok) {
             throw new Error('Network response was not ok');
         }
-  
+
         const predictions = await response.json();
-        displayResults(predictions);
+        // Сохраняем результаты в localStorage
+        localStorage.setItem('predictions', JSON.stringify(predictions));
+        alert('Файлы успешно загружены и обработаны. Нажмите "Перейти к анализу снимков" для просмотра результатов.');
     } catch (error) {
         console.error('Error uploading files:', error);
+        alert('Ошибка при загрузке файлов. Пожалуйста, попробуйте еще раз.');
     }
-  });
-  
-  function displayResults(predictions) {
-    const resultsDiv = document.getElementById('results');
-    resultsDiv.innerHTML = '';
-  
-    predictions.forEach(prediction => {
-        const img = document.createElement('img');
-        img.src = `data:image/png;base64,${prediction.processed_image_base64}`;
-        img.alt = prediction.file_name;
-  
-        const p = document.createElement('p');
-        p.textContent = `File: ${prediction.file_name}, Number of boxes: ${prediction.num_boxes}`;
-  
-        resultsDiv.appendChild(p);
-        resultsDiv.appendChild(img);
-    });
-  }
+});
 
-
-
-
-
+document.getElementById('activeButton').addEventListener('click', function(event) {
+    event.preventDefault(); // Предотвращаем стандартное поведение кнопки
+    reloadPage();
+});
 
 function reloadPage() {
-  location.reload();
+    location.reload();
 }
+
+function displayResults() {
+    const resultsDiv = document.getElementById('results');
+    resultsDiv.innerHTML = ''; // Очищаем предыдущие результаты
+
+    const predictions = JSON.parse(localStorage.getItem('predictions'));
+    if (predictions) {
+        predictions.forEach(prediction => {
+            const img = document.createElement('img');
+            img.src = `data:image/png;base64,${prediction.processed_image_base64}`;
+            img.className = 'carousel-image';
+            img.style.maxWidth = `${imageWidth}px`;
+            img.addEventListener('click', function() {
+                modal.style.display = 'block';
+                modalImg.src = this.src;
+            });
+            resultsDiv.appendChild(img);
+        });
+
+        images = document.querySelectorAll(".carousel-image");
+        totalImages = images.length;
+
+        // Инициализируем карусель
+        updateCarousel();
+    } else {
+        resultsDiv.innerHTML = '<p>Нет результатов для отображения.</p>';
+    }
+}
+
+function updateCarousel() {
+    const offset = currentIndex * (imageWidth + 20);
+    carousel.style.transform = `translateX(-${offset}px)`;
+    carousel.classList.add("sliding-transition");
+
+    setTimeout(() => {
+        carousel.classList.remove("sliding-transition");
+    }, 490);
+}
+
+// Загружаем результаты при загрузке страницы
+window.onload = displayResults;
--- src/frontend/sber_project/src/main.js
+++ src/frontend/sber_project/src/main.js
@@ -33,6 +33,7 @@ document.getElementById('uploadForm').addEventListener('submit', async function(
 
   // Отправляем файлы на сервер
   try {
+      alert('Соединение устанавливается, пожалуйста подождите ответа');
       const response = await fetch('http://127.0.0.1:8000/api/predict/', {
           method: 'POST',
           body: formData
@@ -43,26 +44,48 @@ document.getElementById('uploadForm').addEventListener('submit', async function(
       }
 
       const predictions = await response.json();
-      displayResults(predictions);
+      // Сохраняем результаты на сервере
+      localStorage.setItem('predictions', JSON.stringify(predictions));
+
+      alert('Файлы успешно загружены и обработаны. Нажмите "Перейти к анализу снимков" для просмотра результатов.');
   } catch (error) {
       console.error('Error uploading files:', error);
+      alert('Ошибка при загрузке файлов. Пожалуйста, попробуйте еще раз.');
   }
 });
 
-function displayResults(predictions) {
-  const resultsDiv = document.getElementById('results');
-  resultsDiv.innerHTML = '';
+document.getElementById('activeButton').addEventListener('click', function(event) {
+  event.preventDefault(); // Предотвращаем стандартное поведение кнопки
+  reloadPage();
+});
 
-  predictions.forEach(prediction => {
-      const img = document.createElement('img');
-      img.src = `data:image/png;base64,${prediction.processed_image_base64}`;
-      img.alt = prediction.file_name;
-
-      const p = document.createElement('p');
-      p.textContent = `File: ${prediction.file_name}, Number of boxes: ${prediction.num_boxes}`;
-
-      resultsDiv.appendChild(p);
-      resultsDiv.appendChild(img);
-  });
+function reloadPage() {
+  location.reload();
 }
+
+function displayResults() {
+  const resultsDiv = document.getElementById('results');
+  resultsDiv.innerHTML = ''; // Очищаем предыдущие результаты
+
+  const predictions = JSON.parse(localStorage.getItem('predictions'));
+  if (predictions) {
+      predictions.forEach(prediction => {
+          const img = document.createElement('img');
+          img.src = `data:image/png;base64,${prediction.processed_image_base64}`;
+          img.alt = prediction.file_name;
+
+          const p = document.createElement('p');
+          p.textContent = `File: ${prediction.file_name}, Number of boxes: ${prediction.num_boxes}`;
+
+          resultsDiv.appendChild(p);
+          resultsDiv.appendChild(img);
+      });
+  }
+}
+
+// Загружаем результаты при загрузке страницы
+window.onload = displayResults;
+
+
+
 

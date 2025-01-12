c7858aa4927dd19bdb3bc33d316880b560668f63
colrosezx <kirillkovalev-2005@mail.ru>
2025-01-13 01:02:12 +0500

    add values in table file_name + count

--- src/frontend/sber_project/src/imgs.js
+++ src/frontend/sber_project/src/imgs.js
@@ -15,7 +15,7 @@ leftArrow.addEventListener("click", () => {
     if (currentIndex > 0 && totalImages > 4) {
         currentIndex--;
         updateCarousel();
-    }
+    };
 });
 
 rightArrow.addEventListener("click", () => {
@@ -74,6 +74,7 @@ document.getElementById('uploadForm').addEventListener('submit', async function(
         // Сохраняем результаты в localStorage
         localStorage.setItem('predictions', JSON.stringify(predictions));
         alert('Файлы успешно загружены и обработаны. Нажмите "Перейти к анализу снимков" для просмотра результатов.');
+        
     } catch (error) {
         console.error('Error uploading files:', error);
         alert('Ошибка при загрузке файлов. Пожалуйста, попробуйте еще раз.');
@@ -83,6 +84,7 @@ document.getElementById('uploadForm').addEventListener('submit', async function(
 document.getElementById('activeButton').addEventListener('click', function(event) {
     event.preventDefault(); // Предотвращаем стандартное поведение кнопки
     reloadPage();
+    fillTableWithPredictions(predictions);
 });
 
 function reloadPage() {
@@ -112,6 +114,9 @@ function displayResults() {
 
         // Инициализируем карусель
         updateCarousel();
+
+        // Заполняем таблицу данными
+        fillTableWithPredictions(predictions);
     } else {
         resultsDiv.innerHTML = '<p>Нет результатов для отображения.</p>';
     }
@@ -136,5 +141,31 @@ function updateCarousel() {
     rightArrow.disabled = currentIndex === Math.floor(totalImages / 2); // Блокируем кнопку справа на последнем изображении
 }
 
-// Загружаем результаты при загрузке страницы
 window.onload = displayResults;
+
+function fillTableWithPredictions(predictions) {
+    // Получаем tbody таблицы
+    const tbody = document.querySelector("tbody");
+
+    // Очищаем таблицу перед добавлением новых данных
+    tbody.innerHTML = '';
+
+    // Проходим по каждому элементу в predictions и добавляем строки в таблицу
+    predictions.forEach(prediction => {
+        const row = document.createElement('tr');
+
+        // Создаем ячейку для названия
+        const nameCell = document.createElement('td');
+        nameCell.textContent = prediction.file_name;
+        row.appendChild(nameCell);
+
+        // Создаем ячейку для num_boxes
+        const numBoxesCell = document.createElement('td');
+        numBoxesCell.className = "metric_count";  // Присваиваем класс для стилизации
+        numBoxesCell.textContent = prediction.num_boxes;
+        row.appendChild(numBoxesCell);
+
+        // Добавляем строку в tbody
+        tbody.appendChild(row);
+    });
+}
